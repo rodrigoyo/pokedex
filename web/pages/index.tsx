acf8049effcc25e3e9641c0e765d74148de7e9d9
ryoshii <rodrigo.yoshioka@gmail.com>
2020-09-13 14:47:58 -0300

    feat: Connected with api pokemon

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Head from '../src/infra/components/Head'
 import Header from '../src/pattern/Header'
 
+interface IPokemon {
+  name: string
+}
+
 const Home: React.FC = () => {
+  const [pokemons, setPokemons] = useState<IPokemon[]>([])
+  useEffect(() => {
+    async function loadPokemons() {
+      const response = await axios.get(
+        'https://pokeapi.co/api/v2/pokemon?limit=5'
+      )
+
+      setPokemons(response.data.results)
+    }
+
+    loadPokemons()
+  }, [])
+
   return (
     <>
       <Head>Pokedex</Head>
       <Header />
-      <main>TESTE</main>
+      <main>
+        {pokemons &&
+          pokemons.map((pokemon, i) => {
+            return <h4 key={i}>{pokemon.name}</h4>
+          })}
+      </main>
     </>
   )
 }
