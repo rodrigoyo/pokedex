236fe47516c6b9d9f8b652faded93e08a30f2594
ryoshii <rodrigo.yoshioka@gmail.com>
2020-09-15 18:09:38 -0300

    feat: Getted image pokemon

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,10 +1,13 @@
+import { Flex } from '@chakra-ui/core'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Head from '../src/infra/components/Head'
 import Header from '../src/pattern/Header'
+import PokeCard from '../src/pattern/PokeCard'
 
 interface IPokemon {
   name: string
+  image: string
 }
 
 const Home: React.FC = () => {
@@ -12,10 +15,18 @@ const Home: React.FC = () => {
   useEffect(() => {
     async function loadPokemons() {
       const response = await axios.get(
-        'https://pokeapi.co/api/v2/pokemon?limit=5'
+        'https://pokeapi.co/api/v2/pokemon?limit=10'
       )
+      const pokemonsLoaded = response.data.results.map(({ name, url }) => {
+        const link = new URL(url)
+        const number = link.pathname.split('/')[4]
+        return {
+          name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`
+        }
+      })
 
-      setPokemons(response.data.results)
+      setPokemons(pokemonsLoaded)
     }
 
     loadPokemons()
@@ -25,12 +36,21 @@ const Home: React.FC = () => {
     <>
       <Head>Pokedex</Head>
       <Header />
-      <main>
+      <Flex
+        as="main"
+        wrap="wrap"
+        alignContent="flex-start"
+        alignItems="flex-start"
+        justifyContent="space-between"
+        margin="16px"
+      >
         {pokemons &&
           pokemons.map((pokemon, i) => {
-            return <h4 key={i}>{pokemon.name}</h4>
+            return (
+              <PokeCard key={i} name={pokemon.name} image={pokemon.image} />
+            )
           })}
-      </main>
+      </Flex>
     </>
   )
 }
---
+++ web/src/pattern/PokeCard/index.tsx
@@ -0,0 +1,40 @@
+import { Badge, Box, Image, Flex } from '@chakra-ui/core'
+import React from 'react'
+
+interface IPokemon {
+  name: string
+  image: string
+}
+const PokeCard: React.FC<IPokemon> = ({ name, image }) => {
+  return (
+    <Flex
+      minW="186px"
+      maxW="186px"
+      borderWidth="1px"
+      rounded="lg"
+      overflow="hidden"
+      flexDirection="column"
+    >
+      <Box alignSelf="center">
+        <Image src={image} alt={name} />
+      </Box>
+      <Box p="3">
+        <Box d="flex" alignItems="baseline" justifyContent="center">
+          <Box
+            d="flex"
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+            {name}
+          </Box>
+        </Box>
+      </Box>
+    </Flex>
+  )
+}
+
+export default PokeCard
